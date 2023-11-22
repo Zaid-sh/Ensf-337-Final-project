7f2a72f08b95515df38beb527aab30914b3e4bff
Zaid-sh <144177530+Zaid-sh@users.noreply.github.com>
2023-11-21 23:31:45 -0700

    added a few getter functions and added return functions to the getter functions

---
+++ Final_class.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#include "Final_class.h"
+
+void Seat::set_row(int rowN){
+    
+}
--- Final_class.h
+++ Final_class.h
@@ -6,16 +6,16 @@
 
 class Seat{
     public:
-    int get_row()const;
-    int get_column()const;
-    bool get_occ()const;
-    int get_PassID()const;
-    int get_FID()const;
+    int get_row()const { return row; }
+    int get_column()const  { return column; }
+    bool get_occ()const { return occ; }
+    int get_PassID()const { return PassID; }
+    int get_FID()const { return FID; }
     void set_occ(bool occ);
     void set_PassID(int PassID);
     void set_FID(int FID);
-    void set_row(int r);
-    void set_column(int c);
+    void set_row(int rowN);
+    void set_column(int colN);
     Seat (int row, int column, bool occ, int PassID, int FID);
     ~Seat();
 
@@ -29,8 +29,11 @@ class Seat{
 
 class Passenger{
     public:
-    string get_Fname()const;
-    string get_Lname()const;
+    string get_Fname()const { return Fname; }
+    string get_Lname()const { return Lname; }
+    int get_Phone()const { return Phone; }
+    int get_ID()const { return ID; }
+    int get_FID()const { return FID; }
     void set_Fname(string Fname);
     void set_Lname(string Lname);
     void set_Phone(int Phone);
@@ -43,4 +46,6 @@ class Passenger{
     int Phone;
     int ID;
     int FID;
-}
+};
+
+#endif
