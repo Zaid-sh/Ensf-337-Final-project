04649ad5393b29dad06ba00be26f1ff2e22396c2
Shaheer12345678 <144759340+Shaheer12345678@users.noreply.github.com>
2023-11-27 23:16:30 -0800

    Update flight.h

--- flight.h
+++ flight.h
@@ -1,15 +1,29 @@
 #include <fstream>
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <string>
+#include "Passenger.h"
+#include "Seat.h"
+
 using namespace std;
 
-
 class Flight{
-    public:
+public:
+    Flight(int num_rows, int num_cols);
+    void addPassenger(const Passenger& passenger);
+    void removePassenger(const string& passengerID);
+    Seat* getSeat(const string& seatNumber);
+    void displaySeatMap() const;
+    
 
-    private:
+private:
     int num_rows;
     int num_cols;
-    vector<Passenger> Passengers;
-    vector<vector<seat>seatmap>;
+    vector<Passenger> passengers;
+    vector<vector<Seat>> seatMap;
 };
+
+Flight::Flight(int rows, int cols) : num_rows(rows), num_cols(cols) {
+   
+}
+
